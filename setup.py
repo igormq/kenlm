090a55c92cad5b9d7de36cb669fce3ac0c036c4b
Igor Macedo Quintanilha <igormq@poli.ufrj.br>
2018-07-17 19:40:17 -0300

    Using cythonizing Compilig directly from pyx

--- setup.py
+++ setup.py
@@ -1,51 +1,55 @@
-from setuptools import setup, Extension
 import glob
-import platform
 import os
+import platform
 
-#Does gcc compile with this header and library?
+from Cython.Build import cythonize
+from setuptools import Extension, setup
+
+extra_compile_args = []
+libraries = []
+
+CC = os.environ.get('CC', 'gcc')
+
+
+# Does gcc compile with this header and library?
 def compile_test(header, library):
     dummy_path = os.path.join(os.path.dirname(__file__), "dummy")
-    command = "bash -c \"g++ -include " + header + " -l" + library + " -x c++ - <<<'int main() {}' -o " + dummy_path + " >/dev/null 2>/dev/null && rm " + dummy_path + " 2>/dev/null\""
+    command = "bash -c \"" + CC + " -include " + header + " -l"
+    command += library + " -x c++ - <<<'int main() {}' -o " + dummy_path + " >/dev/null 2>/dev/null && rm "
+    command += dummy_path + " 2>/dev/null\""
     return os.system(command) == 0
 
 
-FILES = glob.glob('util/*.cc') + glob.glob('lm/*.cc') + glob.glob('util/double-conversion/*.cc')
-FILES = [fn for fn in FILES if not (fn.endswith('main.cc') or fn.endswith('test.cc'))]
+files = glob.glob('util/*.cc') + glob.glob('lm/*.cc') + glob.glob('util/double-conversion/*.cc')
+files = [fn for fn in files if not (fn.endswith('main.cc') or fn.endswith('test.cc'))]
 
 if platform.system() == 'Linux':
-    LIBS = ['stdc++', 'rt']
+    libraries += ['stdc++', 'rt']
 elif platform.system() == 'Darwin':
-    LIBS = ['stdc++']
-else:
-    LIBS = []
+    libraries += ['stdc++']
 
-#We don't need -std=c++11 but python seems to be compiled with it now.  https://github.com/kpu/kenlm/issues/86
-ARGS = ['-O3', '-DNDEBUG', '-DKENLM_MAX_ORDER=6', '-std=c++11']
+# We don't need -std=c++11 but python seems to be compiled with it now.  https://github.com/kpu/kenlm/issues/86
+extra_compile_args += ['-O3', '-DNDEBUG', '-DKENLM_MAX_ORDER=6', '-std=c++11']
 
 if compile_test('zlib.h', 'z'):
-    ARGS.append('-DHAVE_ZLIB')
-    LIBS.append('z')
+    extra_compile_args += ['-DHAVE_ZLIB']
+    libraries += ['z']
 
 if compile_test('bzlib.h', 'bz2'):
-    ARGS.append('-DHAVE_BZLIB')
-    LIBS.append('bz2')
+    extra_compile_args += ['-DHAVE_BZLIB']
+    libraries += ['bz2']
 
 if compile_test('lzma.h', 'lzma'):
-    ARGS.append('-DHAVE_XZLIB')
-    LIBS.append('lzma')
+    extra_compile_args += ['-DHAVE_XZLIB']
+    libraries += ['lzma']
 
-ext_modules = [
-    Extension(name='kenlm',
-        sources=FILES + ['python/kenlm.cpp'],
-        language='C++', 
+ext_modules = cythonize(
+    Extension(
+        name='kenlm',
+        sources=files + ['python/kenlm.pyx'],
+        language='c++',
         include_dirs=['.'],
-        libraries=LIBS, 
-        extra_compile_args=ARGS)
-]
+        libraries=libraries,
+        extra_compile_args=extra_compile_args))
 
-setup(
-    name='kenlm',
-    ext_modules=ext_modules,
-    include_package_data=True,
-)
+setup(name='kenlm', ext_modules=ext_modules, include_package_data=True, setup_requires=["cython"])
